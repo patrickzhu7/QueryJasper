629dcd654f57ac1022ed6d8b19413e90b686ccba
patrickzhu7 <pzhu@keste.com>
2019-08-21 11:21:00 -0500

    initial commit

---
+++ src/main/java/com/keste/QueryJasperApplication.java
@@ -0,0 +1,13 @@
+package com.keste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryJasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryJasperApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/keste/ReportGenerator.java
@@ -0,0 +1,5 @@
+package com.keste;
+
+public class ReportGenerator {
+
+}
